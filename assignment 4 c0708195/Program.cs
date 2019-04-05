0dee331a06424f882cbf3e72123abbbd75864474
rupali rupali <rupalimonga456@gmail.com>
2019-04-05 19:28:59 -0400

    commit1

--- assignment 4 c0708195/Program.cs
+++ assignment 4 c0708195/Program.cs
@@ -3,13 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
 
 namespace assignment_4_c0708195
 {
     class Program
     {
+        ArrayList Beowulf;
+
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
         }
+        public void Run() { this.ReadTextFiles(); }
+
+        public void ReadTextFiles()
+        {
+            // Read file using StreamReader. Read file line by line
+            using (StreamReader file = new StreamReader("C:/area51/beowulf.txt"))
+
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+
+                }
+                file.Close();
+                Console.WriteLine("File has {contour) lines.");
+            }
+        }
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            // https://stackoverflow.com/questions/17812566/count-words-and-spaces-in-string-c-sharp
+            int countletters = 0;
+            int countSpaces = 0;
+
+            foreach (char c in line)
+            {
+                if (char.IsLetter(c)) { countletters++; }
+                if (char.IsWhiteSpace(c)) { countletters++; }
+
+            }
+            return countSpaces;
+        }
+
     }
 }
